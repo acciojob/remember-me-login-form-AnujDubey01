48b720f5f844881980b3e39ac001057bf3505225
Anuj Dubey <149352591+AnujDubey01@users.noreply.github.com>
2025-07-01 22:54:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+
+let user  = document.getElementById("username");
+let psswrd = document.getElementById("password");
+let  check = document.getElementById("checkbox");
+const btn = document.getElementById("submit");
+const existingBtn = document.getElementById("existing");
+  // Check if credentials are saved
+    const savedUsername = localStorage.getItem("username");
+    const savedPassword = localStorage.getItem("password");
+
+    if (savedUsername && savedPassword) {
+      // Show the existing user button
+      existingBtn.style.display = "inline-block";
+    }
+
+btn.addEventListener("click", function() {
+	event.preventDefault();
+	
+		if(!check.checked)	{
+			alert(`Logged in as ${user.value}`);
+	} else {
+			  localStorage.setItem("username", user);
+		      localStorage.setItem("password", psswrd);
+			
+			 existingBtn.style.display = "inline-block";
+			
+		}
+	
+});
+
+ existingBtn.addEventListener("click", function() {
+      alert(`Logged in as ${savedUsername.value}`);
+    });
+
